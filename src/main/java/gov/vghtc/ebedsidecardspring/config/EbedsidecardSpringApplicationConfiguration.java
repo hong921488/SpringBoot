e59eab123ddf229ee3fefec608fd02aed4eedeb0
C929 <jhchen@vghtc.gov.tw>
2024-07-12 13:45:17 +0800

    init

---
+++ src/main/java/gov/vghtc/ebedsidecardspring/config/EbedsidecardSpringApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package gov.vghtc.ebedsidecardspring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "gov.vghtc" })
+public class EbedsidecardSpringApplicationConfiguration {
+
+}
